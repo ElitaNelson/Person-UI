96248c48577bff3d9e079051111314daa09af6f3
Elita Nelson <ezn526@mail.missouri.edu>
2019-09-11 21:12:12 -0500

    Completed challenge 3

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Elita Nelson on 9/11/19.
+//  Copyright © 2019 Elita Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
